d7726bb66a188ba135ebb1238821591febc8639c
Mina Samir <mina15g4y@gmail.com>
2025-11-11 15:47:00 +0200

    شرح مختصر للتعديلات اللي عملتها

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n' // Initialize i18next
 import App from './App.jsx'
@@ -10,7 +9,7 @@ import CartProvider from './components/context/CartContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+  <BrowserRouter basename="/blue-ecommerce-main">
       <CartProvider>
         <App />
       </CartProvider>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Set base to repository name for correct asset paths when deployed to GitHub Pages
+  base: "/blue-ecommerce-main/",
   plugins: [react()],
 })
